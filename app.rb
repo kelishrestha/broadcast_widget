868fd2ab3db5365be5188bcbcd884ebe5ede14b8
Kelina Shrestha <kylesth@gmail.com>
2020-08-29 20:03:08 +0545

    [Feature] View broadcast widget (#2)
    * Create broadcast widget pages.
    
    * Add not found page and font.

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ class App < Sinatra::Base
   register Sinatra::R18n
   R18n.default_places { './locales' }
   R18n.set('en')
+  enable :sessions
 
   configure do
     set :views, "views"
@@ -17,7 +18,46 @@ class App < Sinatra::Base
     erb :not_found
   end
 
+  error Sinatra::NotFound do
+    erb :not_found
+  end
+
   get '/' do
+    clear_widget_code
     erb :index
+  end
+
+  get '/widget_form' do
+    clear_widget_code
+    @type = params['type'] || 'realtime'
+    erb :widget_form
+  end
+
+  post '/widget' do
+    save_content('widget_code', params['content'])
+    redirect '/watch_broadcast'
+  end
+
+  get '/watch_broadcast' do
+    @content = page_content('widget_code')
+    erb :watch_broadcast
+  end
+
+  private
+
+  def page_content(title)
+    File.read("pages/#{title}.txt")
+  rescue Errno::ENOENT
+    return nil
+  end
+
+  def save_content(title, content)
+    File.open("pages/#{title}.txt", "w") do |file|
+      file.print(content)
+    end
+  end
+
+  def clear_widget_code
+    save_content('widget_code', '')
   end
 end
